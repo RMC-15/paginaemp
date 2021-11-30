a9786c7a43fcda3bd662f3b27f810d716c875ed0
Royer Mora Cortes <rmorac@frd.org.mx>
2021-11-30 10:16:05 -0600

    Componente de operaciones (falta vincular con otras pag)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { Empeno1Component } from './pages/empeno1/empeno1.component';
+import { DesempenoComponent } from './pages/desempeno/desempeno.component';
+import { Empeno2Component } from './pages/empeno2/empeno2.component';
+import { Empeno3Component } from './pages/empeno3/empeno3.component';
+import { OperacionesComponent } from './pages/operaciones/operaciones.component';
+import { Pago1Component } from './pages/pago1/pago1.component';
+import { Pago2Component } from './pages/pago2/pago2.component';
+import { Pago3Component } from './pages/pago3/pago3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    Empeno1Component,
+    DesempenoComponent,
+    Empeno2Component,
+    Empeno3Component,
+    OperacionesComponent,
+    Pago1Component,
+    Pago2Component,
+    Pago3Component
   ],
   imports: [
     BrowserModule,
